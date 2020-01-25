0d1c067534095f23a75bfda33325138b31e029de
JamesOdera <nyangoma.odera@gmail.com>
2020-01-25 14:34:41 +0300

    add changes to the test class

--- user.py
+++ user.py
@@ -1,13 +1,18 @@
+import pyperclip
+
+
 class User:
     """
-    A class that generates new instances for user details
+    Class that generates new instances of contacts.
     """
 
-user_list = [] 
+    user_list = [] 
 
-def __init__(self,first_name,last_name,phone_number,email):
+    def __init__(self,first_name,last_name,password,email):
+
+
 
         self.first_name = first_name
         self.last_name = last_name
-        self.phone_number = number
+        self.password = password
         self.email = email
--- user_test.py
+++ user_test.py
@@ -1,30 +1,30 @@
-import unittest # Import unit test after creating a user class
+import unittest # Importing the unittest module
+import pyperclip
 from user import User # Importing the user class
 
 class TestUser(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the user class behaviours.
+    Test class that defines test cases for user class
 
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
 
     def setUp(self):
         '''
-        Set up method to run before each test cases.
+        Set method to run before test classes
         '''
-        self.new_user = User("James","Odera","0701740229","nyangoma.odera@gmail.com") # create user object
+        self.new_user = User("James","Odera","6656","nyangoma.odera@gmail.com") # create user object
 
 
     def test_init(self):
         '''
-        test_init test case to test if the object is initialized properly
+        test_init_ to test if the object is initialized well
         '''
 
         self.assertEqual(self.new_user.first_name,"James")
         self.assertEqual(self.new_user.last_name,"Odera")
-        self.assertEqual(self.new_user.phone_number,"0701740229")
+        self.assertEqual(self.new_user.password,"6656")
         self.assertEqual(self.new_user.email,"nyangoma.odera@gmail.com")
 
 
