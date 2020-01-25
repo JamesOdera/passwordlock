b67a5f0d7507a5574193a3ba1aa6ab5a8a6dffa2
JamesOdera <nyangoma.odera@gmail.com>
2020-01-25 16:04:59 +0300

    add a function to create new password

--- password.py
+++ password.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3.6
+
+from user import User
+
+
+def create_user(fname,lname,password,email):
+    '''
+    Fxn to create a new user
+    '''
+    new_user = User(fname,lname,password,email)
+    return new_user
