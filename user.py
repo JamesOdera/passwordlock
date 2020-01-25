8d92634c8574c7b11d309c2d7d201f399242f874
JamesOdera <nyangoma.odera@gmail.com>
2020-01-25 13:59:34 +0300

    create the user file  and add a user class and the arguments

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    A class that generates new instances for user details
+    """
+
+user_list = [] 
+
+def __init__(self,first_name,last_name,phone_number,email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
